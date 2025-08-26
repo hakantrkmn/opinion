d66f3478683ac2ae9121191f48c38302a2c760dc
Hakan Türkmen <hakannturkmen@gmail.com>
2025-08-26 10:36:48 +0300

    spped analtyc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import { QueryProvider } from "@/components/providers/QueryProvider";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
 import { AuthProvider } from "@/contexts/AuthContext";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "maplibre-gl/dist/maplibre-gl.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
@@ -39,6 +41,8 @@ export default function RootLayout({
             disableTransitionOnChange
           >
             <AuthProvider>
+              <Analytics />
+              <SpeedInsights />
               {children}
               <Toaster />
             </AuthProvider>
